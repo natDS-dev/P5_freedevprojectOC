16b1c84d333651afe5663cb9b1ac531b5eaf4f0a
natDS-dev <natachadesmet@yahoo.fr>
2020-06-01 18:55:55 +0200

    Suite structure, routeur base

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../vendor/autoload.php';
-use App\Controllers\HomeController;
 
-$controller = new HomeController();
-$controller->error();
+$controller = $_GET["controller"];
+$action = $_GET["action"];
+$id = isset($_GET["id"]) ? (int)$_GET["id"]:0;
+$controller = "App\\Controllers\\Front\\".ucfirst($controller)."Controller";
+(new $controller())->$action($id);
