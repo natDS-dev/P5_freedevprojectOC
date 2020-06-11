a302f8db07e16f3a04fbc3b0ccbd2499036f3458
natDS-dev <natachadesmet@yahoo.fr>
2020-06-11 11:23:38 +0200

    routeur terminé + tests css

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
 <?php
 session_start();
 require_once '../vendor/autoload.php';
+use App\Controllers\HomeController;
 
-$controller = $_GET["controller"];
-$action = $_GET["action"];
-$id = isset($_GET["id"]) ? (int)$_GET["id"]:0;
-$controller = "App\\Controllers\\Front\\".ucfirst($controller)."Controller";
-(new $controller())->$action($id);
+$controller = isset($_GET["controller"]) ? $_GET["controller"] : "";
+if(!empty($controller))
+{
+    $controller = "App\\Controllers\\".ucfirst($controller)."Controller";
+}
+
+if(class_exists($controller))
+{
+    $action = isset($_GET["action"])? $_GET["action"] : "";
+    if(method_exists($controller,$action))
+    {
+        $id = isset($_GET["id"]) ? (int)$_GET["id"]:0;
+        (new $controller())->$action($id);
+        exit;
+    }
+}   
+(new HomeController())->error(); 
